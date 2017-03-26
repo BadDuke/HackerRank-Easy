d1303f5a22c713fa2a7704f898f0b60df5a2123f
Ben Mantle <BadDuke@users.noreply.github.com>
2017-03-25 22:33:06 -0400

    Add link to camelCase and some formatting

--- camelCase.cpp
+++ camelCase.cpp
@@ -21,21 +21,26 @@
 #include <algorithm>
 #include <unordered_map>
 
+// https://www.hackerrank.com/challenges/camelcase
+
+/************************************************/
+
 using namespace std;
 
 int numberOfWords(string word) {
 	// returns the number of words contained in a camelCase string
 	
-    /* Constraint: string isn't empty, therefore total is at least one.
+	/* Constraint: string isn't empty, therefore total is at least one.
 	   Checking for uppercase characters won't spot the first word in camelCase.
 	   Therefore we set total to 1, then check for uppercase letters from there.
 	*/
+	
 	int total = 1;
-    for (int i = 0; i < word.length(); i++) {
-        if (isupper(word[i])) {
-            total++;
-        }
-    }
+    	for (int i = 0; i < word.length(); i++) {
+        	if (isupper(word[i])) {
+            		total++;
+        	}
+    	}
     return total;
 }
 
