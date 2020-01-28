6407755187537722a2cc465152c062e647a929ec
Benjamin Mantle <BadDuke@users.noreply.github.com>
2020-01-27 21:52:04 -0500

    Spacing update

--- SQL (MS SQL Server)/The Report/TheReport.sql
+++ SQL (MS SQL Server)/The Report/TheReport.sql
@@ -1,9 +1,14 @@
-SELECT CASE
-            WHEN g.Grade < 8 THEN NULL
-            ELSE s.Name
-        END AS Name
-      , g.Grade
-      , s.Marks
+SELECT
+      CASE
+          WHEN g.Grade < 8 THEN NULL
+          ELSE s.Name
+      END AS Name
+    , g.Grade
+    , s.Marks
 FROM Students s
-INNER JOIN Grades g ON s.Marks BETWEEN g.Min_Mark AND g.Max_Mark
-ORDER BY g.Grade DESC, s.Name, s.Marks;
+INNER JOIN Grades g
+      ON s.Marks BETWEEN g.Min_Mark AND g.Max_Mark
+ORDER BY
+      g.Grade DESC
+    , s.Name
+    , s.Marks;
